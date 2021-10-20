0541b3070f28fe2e856d05c199aad040699f07b3
anbunathan <anbunathan.r@gmail.com>
2021-10-20 10:22:06 +0530

    index.js is updated to enable deployment

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Promise from "bluebird";
 
 import auth from "./routes/auth";
 import users from "./routes/users";
+const cors = require('cors');
 
 dotenv.config();
 const app = express();
@@ -21,8 +22,15 @@ app.post("/api/auth", (req,res) => {
     res.status(400).json({errors: {global:"Invalid credentials"}});
 });
 
-app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+// app.get("/*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "index.html"));
+//   });
 
-app.listen(8080, () => console.log("Running on localhost:8080"));
+// app.listen(8080, () => console.log("Running on localhost:8080"));
+
+app.use(express.static(path.join(__dirname, '..', 'client/build')));
+console.log("__dirname = %s", path.resolve(path.join(__dirname, '..', 'client/build', 'index.html')));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', 'client/build', 'index.html'));
+})
+app.listen(process.env.PORT || 8080, () => console.log("Running on localhost:8080"));
